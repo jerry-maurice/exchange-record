b557cdefaef519f926431a62f3f8c95c909eaa21
Jerry <tug25060@temple.edu>
2020-08-14 22:11:25 -0400

    fixed  issue with adding new country to db

--- country/views.py
+++ country/views.py
@@ -39,15 +39,39 @@ def addCountry(request):
     if Account.objects.filter(user=user).exists():
         account = get_object_or_404(Account, user=user)
         company = get_object_or_404(Company, account=account)
+        countries =  Country.objects.filter(company=company)
         if request.method == 'GET':
-            countries =  Country.objects.filter(company=company)
             context = {
                 'countries':countries,
                 'company':company,
             }
             return  render(request,  'country/add/countryadd.html',context)
         if request.method == 'POST':
-            try:
+            country_name =  request.POST['country']
+            search_url = 'https://restcountries.eu/rest/v2/name/'+country_name
+            response = requests.get(search_url)
+            if response.status_code == 200:
+                data = response.json()
+                name = data[0]['name']
+                alpha2Code = data[0]['alpha2Code']
+                alpha3Code = data[0]['alpha3Code']
+                currency = data[0]['currencies'][0]['name']
+                symbol = data[0]['currencies'][0]['symbol']
+                flag = data[0]['flag']
+                language = data[0]['languages'][0]['nativeName']
+                if Country.objects.filter(name=name, company=company).exists() == False:
+                    country = Country(name=name, alpha2Code=alpha2Code, alpha3Code=alpha3Code, 
+                                currency=currency, flag=flag, language=language, company=company)
+                    country.save()
+            else:
+                context = {
+                    'countries':countries,
+                    'company':company,
+                    'message':'The country that you specified was not found'
+                }
+                return  render(request,  'country/add/countryadd.html',context)
+            return redirect(addCountry)
+            '''try:
                 json_data = json.loads(request.body)
                 name = json_data['name']
                 alpha2Code = json_data['alpha2Code']
@@ -60,7 +84,7 @@ def addCountry(request):
                 country.save()
             except json.decoder.JSONDecodeError:
                 logger.warning("String could not be converted to JSON")
-            return redirect(addCountry)
+            return redirect(addCountry)'''
     else:
         logout(request)
         context = {
